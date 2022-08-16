cac592ad5604703f508937cd182109f2dadd15c4
Sarbjeet <97713973+sarbjeet7@users.noreply.github.com>
2022-08-16 02:39:23 -0400

    server files added

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const UserModel = require("./models/Users");
+
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://sarbjeet:sarbjeet123@cluster0.x3mhymw.mongodb.net/student?retryWrites=true&w=majority"
+  );
+
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/createUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
+});
+
+app.listen(3001, () => {
+  console.log("SERVER RUNS PERFECTLY!");
+});
